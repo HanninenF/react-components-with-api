c1df3b4b6d5126a130acc5cba8e2f6b1a0d886e8
HanninenF <hanninenfredrik@gmail.com>
2025-03-01 15:36:11 +0100

    add: SearchBar 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { AllTypes } from "./types/types";
-import { SearchBar } from "./Widgets/SearchBar/SearchBar";
+/* import { SearchBar } from "./Widgets/SearchBar/SearchBar"; */
 import { MovieMiniCard } from "./Widgets/MovieMiniCard/MovieMiniCard";
+import { SearchBar } from "./Widgets/SearchBar2/SearchBar";
 
 export function App() {
   const [movies, setMovies] = useState<AllTypes.Search[] | null>(null);
 
   return (
     <>
-      <SearchBar setMovies={setMovies} />
-      <MovieMiniCard movies={movies} />
+      {/* <SearchBar setMovies={setMovies} />
+      <MovieMiniCard movies={movies} /> */}
+      <SearchBar />
     </>
   );
 }
